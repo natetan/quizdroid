7a471e8acae38992c722da41ae46632bbadb26b5
Yulong Tan <yulongtan67@gmail.com>
2017-01-29 12:20:41 -0800

    add results page

--- app/src/main/java/edu/washington/swifties/quizdroid/QuestionActivity.java
+++ app/src/main/java/edu/washington/swifties/quizdroid/QuestionActivity.java
@@ -1,6 +1,7 @@
 package edu.washington.swifties.quizdroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,25 +14,38 @@ public class QuestionActivity extends Activity {
   private RadioButton choice1, choice2, choice3, choice4;
   private TextView questionTextView;
   private Button submitButton;
-  private boolean selected = false;
+  private int answerSelected = -1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_question);
+
+    submitButton = (Button) findViewById(R.id.submitButton);
+    submitButton.setVisibility(View.INVISIBLE);
     choices = (RadioGroup) findViewById(R.id.choices);
+    choices.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        submitButton.setVisibility(View.VISIBLE);
+        answerSelected = i;
+      }
+    });
+
     RadioButton[] rbs = {choice1, choice2, choice3, choice4};
     int[] ids = {R.id.choice1, R.id.choice2, R.id.choice3, R.id.choice4};
 
     for (int i = 0; i < rbs.length; i++) {
       rbs[i] = (RadioButton) findViewById(ids[i]);
+
     }
 
-    submitButton = (Button) findViewById(R.id.submitButton);
     submitButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-
+        Intent intent = new Intent(QuestionActivity.this, ResultsActivity.class);
+        intent.putExtra("ANSWER SELECTED", answerSelected);
+        startActivity(intent);
       }
     });
   }
---
+++ app/src/main/java/edu/washington/swifties/quizdroid/ResultsActivity.java
@@ -0,0 +1,13 @@
+package edu.washington.swifties.quizdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultsActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_results);
+  }
+}
