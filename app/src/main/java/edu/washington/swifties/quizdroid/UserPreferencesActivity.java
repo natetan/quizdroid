abe2495e6e9f8ce4df35a0e89f717e5d5a2455c5
Yulong Tan <yulongtan67@gmail.com>
2017-02-15 01:20:00 -0800

    add menus and preferences

---
+++ app/src/main/java/edu/washington/swifties/quizdroid/UserPreferencesActivity.java
@@ -0,0 +1,13 @@
+package edu.washington.swifties.quizdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserPreferencesActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_user_preferences);
+  }
+}
